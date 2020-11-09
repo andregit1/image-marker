866ab9bade98d00da958566ee6ffd28da36034f5
Febri Milansyah <febrimilan00@gmail.com>
2020-11-09 16:36:13 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      x: 0,
+      y: 0,
+      markers: [],
+    }
+  }
+
+  handleMouseMove = (e) => {
+    this.setState({
+      x: e.clientX,
+      y: e.clientY,
+    })
+  }
+
+  handleClick = (e) => {
+    const rel = e.target.getBoundingClientRect()
+
+    const newMarker = {
+      x: this.state.x - rel.left - 15,
+      y: this.state.y - rel.top - 15,
+    }
+
+    this.setState(prevState => ({
+      markers: [...prevState.markers, newMarker],
+    }))
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div className="img-box" onMouseMove={this.handleMouseMove} onClick={this.handleClick}>
+          {this.state.markers.map((value, index) => {
+            return (
+              <div
+                key={index}
+                style={{
+                  left: value.x,
+                  top: value.y,
+                }}
+                className="marker"
+              >
+                {index + 1}
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
